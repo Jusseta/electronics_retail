190a26c9d67cb344762faed2c7f4df4f192a682d
Victoria Kashpur <vikki0011@gmail.com>
2024-01-13 15:54:38 +1000

    filter for city

--- chain/admin.py
+++ chain/admin.py
@@ -3,21 +3,22 @@ from chain.models import RetailChain, Contacts, Product
 
 
 @admin.register(Contacts)
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ['email', 'country', 'city', 'street', 'building']
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ('email', 'country', 'city', 'street', 'building',)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'model', 'release_date']
+    list_display = ('name', 'model', 'release_date',)
 
 
 @admin.register(RetailChain)
-class NetworkElementAdmin(admin.ModelAdmin):
-    list_display = ['name', 'contacts', 'provider', 'debt', 'created_at']
-    list_display_links = ['name', 'provider']
-    actions = ["reset_debt"]
+class RetailChainAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contacts', 'provider', 'debt',)
+    list_filter = ('contacts__city',)
+    list_display_links = ('name', 'provider',)
+    actions = ('reset_debt',)
 
-    @admin.action(description="clears the debt to the supplier for selected objects")
+    @admin.action(description='reset the debt')
     def reset_debt(self, request, queryset):
         queryset.update(debt=0.00)
--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chain/', include('chain.urls', namespace='chain')),
+    path('', include('chain.urls', namespace='chain')),
 ]
